e875d762319e05306be33ad4e566dbb36933dfdb
Tran Thi My Duyen <tranthimyduyen702@gmail.com>
2018-08-22 16:12:08 +0700

    delete

--- js/JavaS_vi.js
+++ js/JavaS_vi.js
@@ -14,15 +14,3 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
