a141f61c2bae07e436fe9af8005cbad8529d63dd
Tran Thi My Duyen <tranthimyduyen702@gmail.com>
2018-08-28 11:33:24 +0700

    test

--- js/active.js
+++ js/active.js
@@ -1,12 +1,12 @@
 (function ($) {
-	var $window = $(window);
+	/*var $window = $(window);
 
     // :: 11.0 Preloader active code
     $window.on('load', function () {
         $('#preloader').fadeOut('fast', function () {
             $(this).remove();
         });
-    });
+    });*/
 	
     'use strict';
 
