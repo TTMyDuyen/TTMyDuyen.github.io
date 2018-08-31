d087b3db1d9af7b9892866a55087d986e26dfe2f
Tran Thi My Duyen <tranthimyduyen702@gmail.com>
2018-08-31 09:51:05 +0700

    test

--- js/active.js
+++ js/active.js
@@ -124,13 +124,13 @@
         $('.item').matchHeight();
     }
 
-   /* var $window = $(window);
+    var $window = $(window);
 
     // :: 11.0 Preloader active code
     $window.on('load', function () {
         $('#preloader').fadeOut('fast', function () {
             $(document).remove();
         });
-    });*/
+    });
 
 })(jQuery);
